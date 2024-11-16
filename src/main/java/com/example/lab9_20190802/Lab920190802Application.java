fa656a5a483dedf55900a7061571321c840e523d
brighit0805 <a20190802@pucp.edu.pe>
2024-11-16 18:40:41 -0500

    Initial commit

---
+++ src/main/java/com/example/lab9_20190802/Lab920190802Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20190802;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920190802Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920190802Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab9_20190802/Lab920190802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20190802;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920190802ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
